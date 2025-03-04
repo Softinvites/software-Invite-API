0251b586d498cab5dc2477ef34eaa07c58681b31
Stanley <offorstanley4real@gmail.com>
2025-03-04 08:56:53 +0100

    updated the index.ts

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.post("/admin_register", registerAdmin);
 router.post("/admin_login", loginAdmin);
-router.get("/admin_profile", auth, getAdminProfile);
+router.get("/admin_profile/:id", auth, getAdminProfile);
 router.get("/all_admin_profile", auth, getAllAdminProfile);
 router.put("/update_admin_profile", auth, updateAdminProfile);
 router.put("/update_admin_password", auth, updateAdminPassword);
