b6d46066beceae4ddfbea173504f4bdf10370074
Stanley <offorstanley4real@gmail.com>
2025-04-23 08:32:44 +0100

    optimized the file upload end-point

--- dist/routes/guestRoutes.js
+++ dist/routes/guestRoutes.js
@@ -15,6 +15,7 @@ router.put("/update-guest/:id", auth_1.default, guestController_1.updateGuest);
 router.post("/import-guest-csv", uploadImage_1.uploadCSVExcel.single("file"), auth_1.default, guestController_1.importGuests);
 router.get("/download-qrcode/:id", auth_1.default, guestController_1.downloadQRCode);
 router.get("/download-all-qrcode/:eventId", auth_1.default, guestController_1.downloadAllQRCodes);
+router.get("/batch-qrcode-download/:eventId/timestamp", auth_1.default, guestController_1.downloadBatchQRCodes);
 // routhers others can access temporarily
 router.post("/scan-qrcode", combinedAuth_1.combinedAuth, guestController_1.scanQRCode);
 router.get("/events-guest/:eventId", combinedAuth_1.combinedAuth, guestController_1.getGuestsByEvent);
@@ -22,6 +23,7 @@ router.get("/single-guest/:id", combinedAuth_1.combinedAuth, guestController_1.g
 //other routes
 router.delete("/single-guest/:id", auth_1.default, guestController_1.deleteGuestById);
 router.delete("/event-guest/:eventId", auth_1.default, guestController_1.deleteGuestsByEvent);
+router.delete("/delete/:eventId/timestamp", auth_1.default, guestController_1.deleteGuestsByEventAndTimestamp);
 router.get("/get-analytics/", auth_1.default, guestController_1.generateAnalytics);
 router.post("/generate-temp-link/:eventId", auth_1.default, guestController_1.generateTempLink);
 exports.default = router;
