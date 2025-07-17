735a6bf410577a3fa397352232e2f11c316aee31
Stanley <offorstanley4real@gmail.com>
2025-07-17 01:14:58 +0100

    modified create guest logic

--- dist/utils/utils.js
+++ dist/utils/utils.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateGuestSchema = exports.createGuestSchema = exports.updateEventSchema = exports.creatEventSchema = exports.option = exports.UpdatePasswordSchema = exports.updateAdminProfileSchema = exports.LoginAdminSchema = exports.RegisterAdminSchema = void 0;
+exports.updateGuestSchema = exports.createGuestSchema = exports.updateEventSchema = exports.createEventSchema = exports.option = exports.UpdatePasswordSchema = exports.updateAdminProfileSchema = exports.LoginAdminSchema = exports.RegisterAdminSchema = void 0;
 const joi_1 = __importDefault(require("joi"));
 exports.RegisterAdminSchema = joi_1.default.object({
     username: joi_1.default.string().required(),
@@ -56,7 +56,7 @@ exports.option = {
         },
     },
 };
-exports.creatEventSchema = joi_1.default.object({
+exports.createEventSchema = joi_1.default.object({
     name: joi_1.default.string().required(),
     date: joi_1.default.string().required(),
     location: joi_1.default.string().required(),
