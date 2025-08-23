cac0f6215aae7300ec74347f6f417a9e24a895c6
Stanley Offor <offorstanley4real@gmail.com>
2025-08-23 10:11:24 +0100

    resolving build errors

--- dist/utils/awsConfig.js
+++ dist/utils/awsConfig.js
@@ -1,14 +1,14 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.lambda = exports.s3 = void 0;
-const aws_sdk_1 = __importDefault(require("aws-sdk"));
-aws_sdk_1.default.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+const client_s3_1 = require("@aws-sdk/client-s3");
+const client_lambda_1 = require("@aws-sdk/client-lambda");
+const config = {
     region: process.env.AWS_REGION,
-});
-exports.s3 = new aws_sdk_1.default.S3();
-exports.lambda = new aws_sdk_1.default.Lambda();
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+};
+exports.s3 = new client_s3_1.S3Client(config);
+exports.lambda = new client_lambda_1.LambdaClient(config);
