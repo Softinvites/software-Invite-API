c0f7c310caf45ce7557d8ccc1b3d50ec8c8966dc
Stanley Offor <offorstanley4real@gmail.com>
2025-08-23 23:54:30 +0100

    trying to resolve aws credentials issue

--- dist/utils/awsConfig.js
+++ dist/utils/awsConfig.js
@@ -3,12 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.lambda = exports.s3 = void 0;
 const client_s3_1 = require("@aws-sdk/client-s3");
 const client_lambda_1 = require("@aws-sdk/client-lambda");
+const credential_provider_node_1 = require("@aws-sdk/credential-provider-node");
 const config = {
-    region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
+    region: process.env.AWS_REGION || "us-east-2",
+    credentials: (0, credential_provider_node_1.defaultProvider)(),
 };
 exports.s3 = new client_s3_1.S3Client(config);
 exports.lambda = new client_lambda_1.LambdaClient(config);
